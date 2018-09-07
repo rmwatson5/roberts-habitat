00ecd2f56285a3eb3c0b4f4938dec5be98556b99
Robert Watson <rmwatson5@hotmail.com>
2018-09-07 15:50:33 -0600

    updated web project

--- src/Project/code/Global.asax.cs
+++ src/Project/code/Global.asax.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Sitecore.Project.Website
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : Web.Application
     {
         protected void Application_Start()
         {
