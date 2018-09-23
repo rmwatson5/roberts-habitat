5804d8fe34216fe74d6f302fa9aa314c45904858
Robert Watson <rmwatson5@hotmail.com>
2018-09-22 20:20:17 -0600

    - added item listing - got rid of js. gonna roll with my own

--- FED/js/desktop.js
+++ FED/js/desktop.js
@@ -4,21 +4,8 @@ require.config({
     paths: {
         "jquery" : "//code.jquery.com/jquery-3.3.1.min",
         "jquery-private": "patches/jquery.no-conflict",
-        "modernizr" : "//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min",
-        "shuffle" : "//cdnjs.cloudflare.com/ajax/libs/Shuffle/3.1.1/jquery.shuffle.min",
-        "theming" : "vendor/theming.min",
-        "framework" : "vendor/framweorks.min"
     },
     shim: {
-        "shuffle" : {
-            deps: ["jquery", "modernizr"]
-        },
-        "theming" : {
-            deps: ["jquery", "modernizr", "shuffle"]
-        },
-        "framework" : {
-            deps: ["jquery", "modernizr", "shuffle"]
-        }
     },
     map: {
     '*': {
